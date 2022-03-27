a2fefd1384622d66ab9c05837cd4f91138b5e7dd
Armel FAUVEAU <armel.fauveau@globalis-ms.com>
2022-03-27 01:27:48 +0100

    Spoke is my mentor

---
+++ src/IC705MultiMeter.cpp
@@ -0,0 +1,239 @@
+// Copyright (c) F4HWN Armel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+#include <IC705MultiMeter.h>
+#include "image.h"
+#include "functions.h"
+#include "command.h"
+#include "tasks.h"
+
+// Setup
+void setup()
+{
+  uint8_t loop = 0;
+
+  // Debug
+  Serial.begin(115200);
+
+  // Init M5
+  M5.begin(true, false, false, false);
+
+  // Init Led
+  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+
+  // Init Power
+  power();
+
+  // Preferences
+  preferences.begin(NAME);
+  option = preferences.getUInt("option", 2);
+  brightness = preferences.getUInt("brightness", 64);
+
+  // Bin Loader
+  binLoader();
+
+  // Wifi
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED && loop <= 10)
+  {
+    delay(250);
+    loop += 1;
+  }
+
+  Serial.println(WiFi.localIP());
+
+  // Scroll
+  pos = M5.Lcd.width();
+  Sprite.setTextSize(1);  // Font size scaling is x1
+  Sprite.setTextFont(2);  // Font 2 selected
+  Sprite.createSprite(M5.Lcd.width(), 12);
+
+  message = String(NAME) + " V" + String(VERSION) + " by " + String(AUTHOR);
+ 
+  // Start server (for Web site Screen Capture)
+  httpServer.begin();
+
+  // Let's go
+  #if BOARD == CORE2
+    M5.Axp.SetLed(0);
+  #endif
+
+  setBrightness(brightness);
+  M5.Lcd.setRotation(1);
+  M5.Lcd.fillScreen(TFT_BLACK);
+
+  viewGUI();
+
+  CAT.register_callback(callbackBT);
+
+  if (!CAT.begin(NAME))
+  {
+    Serial.println("An error occurred initializing Bluetooth");
+  }
+  else
+  {
+    Serial.println("Bluetooth initialized");
+  }
+
+  // Multitasking task for retreive button
+  xTaskCreatePinnedToCore(
+      button,   // Function to implement the task
+      "button", // Name of the task
+      8192,     // Stack size in words
+      NULL,     // Task input parameter
+      4,        // Priority of the task
+      NULL,     // Task handle
+      1);       // Core where the task should run
+
+  // Multitasking task for retreive scroll
+  /*
+  xTaskCreatePinnedToCore(
+      scroll,   // Function to implement the task
+      "scroll", // Name of the task
+      8192,     // Stack size in words
+      NULL,     // Task input parameter
+      2,        // Priority of the task
+      NULL,     // Task handle
+      1);       // Core where the task should run
+  */
+}
+
+// Main loop
+void loop()
+{
+  static uint8_t alternance = 0;
+  static uint8_t chargeOld = 128;
+  static uint8_t tx = 0;
+  static uint8_t mode = 0;
+  static uint8_t charge = 0;
+  static uint8_t comp = 0;
+
+  if (btConnected == false)
+  {
+    M5.Lcd.setTextDatum(CC_DATUM);
+    M5.Lcd.setFreeFont(&UniversCondensed20pt7b);
+    M5.Lcd.setTextPadding(200);
+    M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+    M5.Lcd.drawString("Need Pairing", 160, 70);
+    vTaskDelay(500);
+    M5.Lcd.drawString("", 160, 70);
+    vTaskDelay(100);
+    Serial.println("Need Pairing");
+  }
+  else
+  {
+    tx = getTX();
+
+    if(charge != chargeOld) {
+      chargeOld = charge;
+      M5.Lcd.setFreeFont(0);
+      M5.Lcd.setTextPadding(35);
+      M5.Lcd.setTextDatum(CR_DATUM);
+      M5.Lcd.setTextColor(TFT_WHITE, TFT_BLACK);
+
+      if(charge == 0) {
+        M5.Lcd.drawString("(10W)", 194, 138);
+      }
+      else {
+        M5.Lcd.drawString("(5W)", 194, 138);
+      }
+    }  
+
+    getFrequency();
+    
+    if(tx == 0) {
+      if(needClear == false) {
+        getALCLevel();
+        clearGUI();
+        for(uint8_t i = 0; i <= 9; i++){
+          leds[i] = CRGB::Black;
+        }
+        FastLED.setBrightness(16);
+        FastLED.show();
+        needClear = true;
+      }
+      getSmeterLevel();
+    }
+    else {
+      if(needClear) {
+        for(uint8_t i = 0; i <= 9; i++){
+          leds[i] = CRGB::Red;
+        }
+        FastLED.setBrightness(16);
+        FastLED.show();
+        needClear = false;
+      }
+
+      getALCLevel();
+      getSWRLevel();
+      getIdLevel();
+      if(mode <= 1 && comp == 1) {
+        getCOMPLevel();
+      }
+      getPowerLevel(charge);
+    }
+
+    switch (alternance)
+    {
+      case 0:
+        charge = getPowerType();
+        if(mode == 5 || mode == 6) {
+          getTone();
+        }
+        else {
+          getTone(false);
+        }
+        getAGC();
+        getAMP();
+        getVdLevel();
+        break;
+
+      case 1:
+        mode = getModeFilter();
+        getNR();
+        getNB();
+        getAN();
+        getRIT();
+        break;
+
+      case 2:
+        if(mode <= 1) {
+          comp = getCOMP();
+        }
+        else {
+          comp = getCOMP(false);
+        }
+        getAF();
+        getSQL();
+        getMIC();
+        break;
+    }
+
+    if(tx == 0) {
+      getSmeterLevel();
+    }
+    else {
+      getPowerLevel(charge);
+    }
+  }
+
+  alternance = (alternance++ < 2) ? alternance : 0;
+
+  if(DEBUG) {
+    Serial.println("----------");
+    Serial.print("Alternance ");
+    Serial.println(alternance);
+  }
+
+  // View battery
+  viewBattery();
+
+  // View baseline
+  viewBaseline();
+
+  // Manage Web Server if enable
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    getScreenshot();
+  }
+}
---
+++ src/settings.h
@@ -0,0 +1,12 @@
+// Copyright (c) F4HWN Armel. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+// M5Stack Board
+#define BOARD BASIC                             // Your M5Stack Board
+
+// IC705 Bluetooth 
+#define IC705_CI_V_ADDRESS 0xA4                 // IC705 CI-V default address 0xA4
+
+// Wifi Config
+#define WIFI_SSID       "YOUR WIFI SSID"        // Your WiFi SSID
+#define WIFI_PASSWORD   "YOUR WIFI PASSWORD"    // Your WiFi Password
